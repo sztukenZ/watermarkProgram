4452fb210c74aa501ea9d3e7a7a54642591995d5
krzysztofsztuk <krzysztof.jerzy.sztuk@gmail.com>
2022-01-12 10:15:03 +0100

    first window

--- gui.py
+++ gui.py
@@ -1,23 +1,32 @@
-import eel
-import wx
-import jinja2 as jinja2
-
-eel.init("web")
-
-# Start the index.html file
-eel.start(
-    'templates/index.html',
-    jinja_templates='templates'
+from PySide6.QtWidgets import (
+    QApplication, QMessageBox, QWidget, QLabel, QPushButton, QVBoxLayout
 )
 
-@eel.expose
-def pythonFunction(wildcard="*"):
-    app = wx.App(None)
-    style = wx.FD_OPEN | wx.FD_FILE_MUST_EXIST
-    dialog = wx.FileDialog(None, 'Open', wildcard=wildcard, style=style)
-    if dialog.ShowModal() == wx.ID_OK:
-        path = dialog.GetPath()
-    else:
-        path = None
-    dialog.Destroy()
-    return path
+app = QApplication([])
+app.setStyle('macos')
+window = QWidget()
+window.setWindowTitle('Watermarking Images')
+window.setGeometry(100, 100, 280, 80)
+window.move(600, 200)
+window.setStyleSheet("background-color: white;")
+v_layout = QVBoxLayout()
+
+btn1 = QPushButton('button 1')
+btn2 = QPushButton('button 2')
+
+v_layout.addWidget(QLabel('hello world'))
+v_layout.addWidget(btn1)
+v_layout.addWidget(btn2)
+
+
+def on_btn1_clicked():
+    alert = QMessageBox()
+    alert.setText('this is an alert msg')
+    alert.exec()
+
+
+btn1.clicked.connect(on_btn1_clicked)
+
+window.setLayout(v_layout)
+window.show()
+app.exec()
