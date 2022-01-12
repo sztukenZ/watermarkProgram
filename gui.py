6fc6f85265d1c953f6bebaeec43626df19ef3bfb
krzysztofsztuk <krzysztof.jerzy.sztuk@gmail.com>
2022-01-12 17:16:23 +0100

    Changing example image works

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import (
-    QApplication, QMessageBox, QWidget, QLabel, QPushButton, QGridLayout
+    QApplication, QMessageBox, QWidget, QLabel, QPushButton, QGridLayout, QFileDialog
 )
 
 app = QApplication([])
@@ -9,20 +9,22 @@ window = QWidget()
 window.setWindowTitle('Watermarking Images')
 window.setGeometry(100, 100, 280, 80)
 window.move(600, 200)
-window.setStyleSheet("background-color: white;")
+# window.setStyleSheet("background-color: white;")
 layout = QGridLayout()
 
 btn1 = QPushButton('Back')
 btn2 = QPushButton('Choose Images')
 btn3 = QPushButton('Clear Images')
 btn4 = QPushButton('Next Step')
+btn1.setEnabled(False)
+btn4.setStyleSheet("background-color: #A3E4DB")
 
 img_label = QLabel()
 pixmap = QPixmap('example_picture.jpeg')
 img_label.setPixmap(pixmap)
 
 # Optional, resize window to image size
-img_label.resize(pixmap.width(),pixmap.height())
+img_label.resize(pixmap.width(), pixmap.height())
 
 layout.addWidget(btn1, 0, 0)
 layout.addWidget(btn2, 0, 1)
@@ -37,7 +39,17 @@ def on_btn1_clicked():
     alert.exec()
 
 
+def choose_images():
+    jpg_filter = "JPEG (*.jpg *.jpeg)"
+    filenames = QFileDialog.getOpenFileNames(filter=jpg_filter)
+    print(filenames)
+    if filenames[0] != []:
+        img_label.setPixmap(QPixmap(filenames[0][0]))
+        img_label.resize(pixmap.width(), pixmap.height())
+
+
 btn1.clicked.connect(on_btn1_clicked)
+btn2.clicked.connect(choose_images)
 
 window.setLayout(layout)
 window.show()
