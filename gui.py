b0e533c2e6dc183bd20dd759166840adf330d809
krzysztofsztuk <krzysztof.jerzy.sztuk@gmail.com>
2022-01-12 10:42:11 +0100

    Basic template create

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
+from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import (
-    QApplication, QMessageBox, QWidget, QLabel, QPushButton, QVBoxLayout
+    QApplication, QMessageBox, QWidget, QLabel, QPushButton, QGridLayout
 )
 
 app = QApplication([])
@@ -9,14 +10,25 @@ window.setWindowTitle('Watermarking Images')
 window.setGeometry(100, 100, 280, 80)
 window.move(600, 200)
 window.setStyleSheet("background-color: white;")
-v_layout = QVBoxLayout()
+layout = QGridLayout()
 
-btn1 = QPushButton('button 1')
-btn2 = QPushButton('button 2')
+btn1 = QPushButton('Back')
+btn2 = QPushButton('Choose Images')
+btn3 = QPushButton('Clear Images')
+btn4 = QPushButton('Next Step')
 
-v_layout.addWidget(QLabel('hello world'))
-v_layout.addWidget(btn1)
-v_layout.addWidget(btn2)
+img_label = QLabel()
+pixmap = QPixmap('example_picture.jpeg')
+img_label.setPixmap(pixmap)
+
+# Optional, resize window to image size
+img_label.resize(pixmap.width(),pixmap.height())
+
+layout.addWidget(btn1, 0, 0)
+layout.addWidget(btn2, 0, 1)
+layout.addWidget(btn3, 0, 2)
+layout.addWidget(btn4, 0, 3)
+layout.addWidget(img_label, 1, 1, 1, 2)
 
 
 def on_btn1_clicked():
@@ -27,6 +39,6 @@ def on_btn1_clicked():
 
 btn1.clicked.connect(on_btn1_clicked)
 
-window.setLayout(v_layout)
+window.setLayout(layout)
 window.show()
 app.exec()
